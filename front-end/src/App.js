a46062f0baf226b282e0154776156c6cdced6c6f
Pyyzz <ndphonghe130315@gmail.com>
2020-08-17 23:44:45 +0700

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Content from "./Component/Content/Content";
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <Content/>
+    )
+  }
+}
+
+export default App;
